2b842d9d2d8fe4f39749ea095259549ca0f3eb23
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-11-04 22:43:30 -0300

    finalizado

--- php/logon.php
+++ php/logon.php
@@ -16,29 +16,21 @@ include 'conexao.php';
 
 
 $sql = "SELECT * FROM tb_user WHERE email = '$email'"; 
-
-
 $query = $conexao->query($sql);
-
 $resultado = $query->fetch_assoc();
 
 
 $email_banco = $resultado['email'];
 $senha_banco = $resultado['senha'];
 
-
-if ($email == $email_banco && $passaword == $senha_banco ) {
-
- header('location: tela inicial.html ');
-
-}
-else {
-
-echo "<script> alert('Usuario ou senha ivalida'); history.black(); </script>";
-
+if ($email == $email_banco &&  $password == $senha_banco) {
+    session_start();
+    $_SESSION['id'] = $resultado['id_usuario'];
+    header('location: ../tela_inicial.php');
+}else {
+    echo "<script> alert('Usuario ou senha Invalida'); history.back(); </script>"; 
 }
 
 ?>
 
 
-weft
---
+++ tela_inicial.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Sistema Financeiro</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <script src="https://kit.fontawesome.com/c0f408d1cc.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div style="color: writer">
+    <?php
+     session_start();
+     if (isset($_SESSION['id'])) {
+       $id = $_SESSION['id'];
+       echo "olá $id";
+        } else{
+         echo "<script>alert('Usuário precisar logar'); history.back();</script>";
+        }  
+    
+    ?>
+    </div>
+
+
+    
+    <div class="tela">
+
+        <h1><Strong>TELA DE LANÇAMENTO</Strong></h1>
+    
+    <p><strong>Seja bem vindo a tela de lançamento!!! escolha uma opção:</strong></p>
+
+    
+    
+    <div style="display: flex; flex-direction: column; align-items:center; gap: 20px; margin-top: 50px;">
+        <a href="categoria.html" class="btn btn-success">Categoria</a>
+        <a href="lançamento.html" class="btn btn-success">Lançamento</a>
+    </div>
+    
+</div>
+
+
+</body>
